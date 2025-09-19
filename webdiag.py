0c32e6b5b0f4e48afbb4afc30b774f0993908865
kivinew <kivinew@gmail.com>
2025-09-19 12:06:23 +0700

    Update

--- webdiag.py
+++ webdiag.py
@@ -167,4 +167,4 @@ def page_not_found(e):
         menu=error_menu, image_path="/images/404.jpg", image_alt="Страница не найдена"), 404
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=6000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
